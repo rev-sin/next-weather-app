de3233ec726f4181aa99873880871db59e5f05fa
Rohith Singothu <rohithsingothu@gmail.com>
2025-04-01 23:57:43 +0530

    changes in /team

---
+++ src/app/(routes)/team/_members/members.ts
@@ -0,0 +1,32 @@
+export const teamMembers = [
+  {
+    name: "Koushik Reddy Chittibala",
+    role: "Tester",
+    rollno: "CB.EN.U4CSE22108",
+    imageUrl: "/images/koushik.jpg",
+  },
+  {
+    name: "Jishnu Hari",
+    role: "Developer",
+    rollno: "CB.EN.U4CSE22122",
+    imageUrl: "/images/jishnu.jpg",
+  },
+  {
+    name: "Lucky Goyal",
+    role: "Developer",
+    rollno: "CB.EN.U4CSE22130",
+    imageUrl: "/images/lucky.jpg",
+  },
+  {
+    name: "Priyadharshini",
+    role: "Tester",
+    rollno: "CB.EN.U4CSE22147",
+    imageUrl: "/images/priya.jpg",
+  },
+  {
+    name: "Revanth Singothu",
+    role: "Scrum Master",
+    rollno: "CB.EN.U4CSE22149",
+    imageUrl: "/images/revanth.jpg",
+  },
+];
--- src/app/(routes)/team/page.tsx
+++ src/app/(routes)/team/page.tsx
@@ -11,38 +11,7 @@ import {
   CardContent,
 } from "@/components/ui/card";
 
-const teamMembers = [
-  {
-    name: "Koushik Reddy Chittibala",
-    role: "Tester",
-    rollno: "CB.EN.U4CSE22108",
-    imageUrl: "/images/koushik.jpg",
-  },
-  {
-    name: "Jishnu Hari",
-    role: "Developer",
-    rollno: "CB.EN.U4CSE22122",
-    imageUrl: "/images/jishnu.jpg",
-  },
-  {
-    name: "Lucky Goyal",
-    role: "Developer",
-    rollno: "CB.EN.U4CSE22130",
-    imageUrl: "/images/lucky.jpg",
-  },
-  {
-    name: "Priyadharshini",
-    role: "Tester",
-    rollno: "CB.EN.U4CSE22147",
-    imageUrl: "/images/priya.jpg",
-  },
-  {
-    name: "Revanth Singothu",
-    role: "Scrum Master",
-    rollno: "CB.EN.U4CSE22149",
-    imageUrl: "/images/revanth.jpg",
-  },
-];
+import { teamMembers } from "./_members/members";
 
 export default function TeamPage() {
   const [selectedMember, setSelectedMember] = useState(teamMembers[0]);
